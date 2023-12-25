32c9ed3e1aa24bde7ae0e8def8542cf36aca043e
Aditya <aditya.1211y@gmail.com>
2023-12-25 17:03:07 +0530

    Added user model and utils

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const {serverConfig, logger}=require('./config');
 const apiRoutes = require('./routes')
 
 const app=express()
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 app.use('/api',apiRoutes)
 
 app.listen(serverConfig.PORT,()=>{
